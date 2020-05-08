bbe257e62738473850e5a08e768b22afbc8b7707
danieldiaz-2502 <daniel_diazvalencia@outlook.com>
2020-05-08 15:20:48 -0500

    .

--- src/main/Cuadro.java
+++ src/main/Cuadro.java
@@ -8,22 +8,14 @@ public class Cuadro implements Runnable {
 	private int posX;
 	private int posY;
 	private int tam;
-	private int velX;
 	private int velY;
-	private int r;
-	private int g;
-	private int b;
 
-	public Cuadro(PApplet app) {
+	public Cuadro(int posX,int posY,int tam, PApplet app) {
 		this.app = app;
-		posX = (int) app.random(0, 399);
-		posY = (int) app.random(0, 399);
-		tam = (int) app.random(50, 80);
-		velX = 5;
+		this.posX = posX;
+		this.posY = posY;
+		this.tam = tam;
 		velY = 5;
-		r = (int) app.random(0, 250);
-		g = (int) app.random(0, 250);
-		b = (int) app.random(0, 250);
 	}
 
 	public void run() {
@@ -39,24 +31,14 @@ public class Cuadro implements Runnable {
 	}
 
 	public void dibujar() {
-		app.fill(r, g, b);
-		app.circle(posX, posY, tam);
+		
+		app.rect(posX,posY,tam,tam);
 
 	}
 
 	public void mover() {
-
-		posX += velX;
-		if(posX > app.width) {
-			velX *= -1;
-		}
-		if(posX < 0) {
-			velX *= -1;
-		}
+		
 		posY += velY;
-		if(posY > app.height) {
-			velY *= -1;
-		}
 		if(posY < 0) {
 			velY *= -1;
 		}
@@ -95,44 +77,12 @@ public class Cuadro implements Runnable {
 		this.tam = tam;
 	}
 
-	public int getVelX() {
-		return velX;
-	}
-
-	public void setVelX(int velX) {
-		this.velX = velX;
-	}
-
 	public int getVelY() {
 		return velY;
 	}
 
 	public void setVelY(int velY) {
 		this.velY = velY;
-	}
-
-	public int getR() {
-		return r;
-	}
-
-	public void setR(int r) {
-		this.r = r;
-	}
-
-	public int getG() {
-		return g;
-	}
-
-	public void setG(int g) {
-		this.g = g;
-	}
-
-	public int getB() {
-		return b;
-	}
-
-	public void setB(int b) {
-		this.b = b;
 	}
 	
 
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,14 +1,12 @@
 package main;
 
-import java.util.ArrayList;
-
 import processing.core.PApplet;
 
 public class Main extends PApplet {
 	
-	public ArrayList<Cuadro> figuras;
-	public int fila;
-	public int columna;
+	Cuadro figura;
+	int fila;
+	int columna;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -16,52 +14,34 @@ public class Main extends PApplet {
 	}
 
 	public void settings() {
-		size(400, 400);
+		size(600, 400);
 	}
 
 	public void setup() {
-		figuras = new ArrayList<Cuadro>();
-		  
+		
 	}
 
 	public void draw() {
 		  background(255, 255, 255);
-		  for(int i = 0; i < figuras; i++) {
-			  
-			    Cuadro temp = new Cuadro(this);
-			    figuras.add(temp);  
-			  }
-		  /*for(int i = 0; i < figuras.size(); i++) {
-		    
-		    figuras.get(i).dibujar();
-		    new Thread(figuras.get(i)).start();
-		    for(int j = 0; j < figuras.size(); j++) {
-			if((dist(figuras.get(i).getPosX(),figuras.get(i).getPosY(), figuras.get(j).getPosX(),figuras.get(j).getPosY()))<25){
-				if (figuras.get(i).getTam()<figuras.get(j).getTam()) {
-		    	figuras.get(j).setR(figuras.get(i).getR());
-		    	figuras.get(j).setG(figuras.get(i).getG());
-		    	figuras.get(j).setB(figuras.get(i).getB());
-		    	figuras.remove(i);
-		    	return;
-		    } 
-		  }
-		    if (figuras.get(i).getTam() == figuras.get(j).getTam()) {
-				if((dist(figuras.get(i).getPosX(),figuras.get(i).getPosY(), figuras.get(j).getPosX()+25,figuras.get(j).getPosY()))<25){
-			    	figuras.get(j).setVelY(figuras.get(j).getVelY()*-1);
-			    	figuras.get(i).setVelY(figuras.get(i).getVelY()*-1);
-			    	figuras.get(j).setVelX(figuras.get(j).getVelX()*-1);
-			    	figuras.get(i).setVelX(figuras.get(i).getVelX()*-1);
-			    } 
-			  }
-		  }
-		  }*/
+		  for( fila = 0; fila < 30 ; fila++) {
+	            for( columna = 0; columna < 20; columna++) {
+	                figura = new Cuadro(20*fila,20*columna,20,this);
+	                figura.dibujar();
+	                figura.mover();
+	                if(fila % 2 == 0 && columna % 2 == 0) {
+	                    fill(0);
+	                }
+	                else {
+	                    fill(255);
+	                }
+	                if((fila % 2!=0)& columna % 2!=0 )  {
+	                    fill(0);
+	                }
+	            }
+	        }
 	}
 	
 	 
 	public void mousePressed() {
-		for(int i = 0; i < fila; i++) {
-		    Cuadro temp = new Cuadro(this);
-		    figuras.add(temp);  
-		  }
 	}
 }
